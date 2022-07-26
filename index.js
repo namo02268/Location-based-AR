1b37d79a67262fcd27d4a07199ad5a15eef27414
namo <namo02268@gmail.com>
2022-07-26 15:57:22 +0900

    [Add] m

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ window.addEventListener('load', () => {
     // 現在地（カメラ）の空間座標を取得
     const currentPos = document.getElementById('camera').object3D.position;
     // currentに座標の値を代入
-    document.getElementById('current').textContent = `現在地の座標 x:${currentPos.x.toFixed(1)}, y:${currentPos.y.toFixed(1)}, z:${currentPos.z.toFixed(1)}`;
+    document.getElementById('current').textContent = `現在地の座標 x:${currentPos.x.toFixed(1)}m, y:${currentPos.y.toFixed(1)}m, z:${currentPos.z.toFixed(1)}m`;
 
     // 目的地（バーミヤン）の空間座標を取得
     const distinationPos = document.getElementById('Bamiyan').object3D.position;;
     // distinationに座標の値を代入
-    document.getElementById('distination').textContent = `目的地の座標 x:${distinationPos.x.toFixed(1)}, y:${distinationPos.y.toFixed(1)}, z:${distinationPos.z.toFixed(1)}`;
+    document.getElementById('distination').textContent = `目的地の座標 x:${distinationPos.x.toFixed(1)}m, y:${distinationPos.y.toFixed(1)}m, z:${distinationPos.z.toFixed(1)}m`;
 
     // currentPosとdistinationPosのベクトル差を計算
     distance = currentPos.distanceTo(distinationPos);
