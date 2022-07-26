78247100a2338b4a1a080eeb5a3505e6218ef75f
namo <namo02268@gmail.com>
2022-07-26 15:39:57 +0900

    [Add] Comment

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 window.addEventListener('load', () => {
+  // 100msごとに実行
   setInterval(() => {
+    // 現在地（カメラ）の空間座標を取得
     const currentPos = document.getElementById('camera').object3D.position;
+    // currentに座標の値を代入
     document.getElementById('current').textContent = `現在地の座標 x:${currentPos.x.toFixed(1)}, y:${currentPos.y.toFixed(1)}, z:${currentPos.z.toFixed(1)}`;
 
+    // 目的地（バーミヤン）の空間座標を取得
     const distinationPos = document.getElementById('Bamiyan').object3D.position;;
+    // distinationに座標の値を代入
     document.getElementById('distination').textContent = `目的地 x:${distinationPos.x.toFixed(1)}, y:${distinationPos.y.toFixed(1)}, z:${distinationPos.z.toFixed(1)}`;
 
+    // currentPosとdistinationPosのベクトル差を計算
     distance = currentPos.distanceTo(distinationPos);
-    document.getElementById('distance').textContent = `目的地までの距離: ${distance}m`
+    // distanceに値を代入
+    document.getElementById('distance').textContent = `目的地までの距離: ${distance.toFixed(1)}m`
   }, 100);
 });
