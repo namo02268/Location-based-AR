20a79c25133b6d1c07184e4f676bdd241b656eb4
namo <namo02268@gmail.com>
2022-07-27 02:22:21 +0900

    [Add] LonLat

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 window.addEventListener('load', () => {
   // 100msごとに実行
   setInterval(() => {
-    // 現在地（カメラ）の空間座標を取得
-    const currentPos = document.getElementById('camera').object3D.position;
-    // currentに座標の値を代入
-    document.getElementById('current').textContent = `現在地の座標 x:${currentPos.x.toFixed(1)}m, y:${currentPos.y.toFixed(1)}m, z:${currentPos.z.toFixed(1)}m`;
+    // カメラ
+    const camera = document.getElementById('camera');
+    const cPos = camera.object3D.position;
+    const cLatLon = camera.components['gps-camera'].currentCoords;
 
-    console.log(document.getElementById('camera').toSource());
+    document.getElementById('cPos').textContent = `x:${cPos.x.toFixed(1)}m, y:${cPos.y.toFixed(1)}m, z:${cPos.z.toFixed(1)}m`;
+    document.getElementById('cLatLon').textContent = `lat:${cLatLon.latitude}, lon:${cLatLon.longitude}`;
 
-    // 目的地（バーミヤン）の空間座標を取得
-    const distinationPos = document.getElementById('Bamiyan').object3D.position;
-    // distinationに座標の値を代入
-    document.getElementById('distination').textContent = `目的地の座標 x:${distinationPos.x.toFixed(1)}m, y:${distinationPos.y.toFixed(1)}m, z:${distinationPos.z.toFixed(1)}m`;
+    // 目的地
+    const distination = document.getElementById('distination');
+    const dPos = distination.object3D.position;
+    const dLatLon = distination.components['gps-entity-place'].attrValue;
 
-    // currentPosとdistinationPosの距離を計算
-    distance = currentPos.distanceTo(distinationPos);
-    // distanceに値を代入
-    document.getElementById('distance').textContent = `目的地までの距離: ${distance.toFixed(1)}m`
+    document.getElementById('dPos').textContent = `x:${dPos.x.toFixed(1)}m, y:${dPos.y.toFixed(1)}m, z:${dPos.z.toFixed(1)}m`;
+    document.getElementById('dLatLon').textContent = `lat:${dLatLon.latitude}, lon:${dLatLon.longitude}`;
+
+    // 距離
+    distance = cPos.distanceTo(dPos);
+    document.getElementById('distance').textContent = `${distance.toFixed(1)}m`
   }, 3000);
 });
