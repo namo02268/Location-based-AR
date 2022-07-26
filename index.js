b6d4fcd024671c6890637f8c3bb5d98e5a5b101d
namo <namo02268@gmail.com>
2022-07-26 15:33:32 +0900

    .toFixed(1)

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 window.addEventListener('load', () => {
   setInterval(() => {
     const currentPos = document.getElementById('camera').object3D.position;
-    document.getElementById('current').textContent = `現在地の座標
-    x:${currentPos.x}, y:${currentPos.y}, z:${currentPos.z}`;
+    document.getElementById('current').textContent = `現在地の座標 x:${currentPos.x.toFixed(1)}, y:${currentPos.y.toFixed(1)}, z:${currentPos.z.toFixed(1)}`;
 
     const distinationPos = document.getElementById('Bamiyan').object3D.position;;
-    document.getElementById('distination').textContent = `目的地: ${distinationPos.x}, ${distinationPos.y}, ${distinationPos.z}`;
+    document.getElementById('distination').textContent = `目的地 x:${distinationPos.x.toFixed(1)}, y:${distinationPos.y.toFixed(1)}, z:${distinationPos.z.toFixed(1)}`;
 
     distance = currentPos.distanceTo(distinationPos);
     document.getElementById('distance').textContent = `目的地までの距離: ${distance}m`
