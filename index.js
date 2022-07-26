eb32537e31cf6a265d4254d31923b918d328527c
namo <namo02268@gmail.com>
2022-07-26 15:30:19 +0900

    [Add] xyz

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', () => {
   setInterval(() => {
     const currentPos = document.getElementById('camera').object3D.position;
-    document.getElementById('current').textContent = `現在地: ${currentPos.x}, ${currentPos.y}, ${currentPos.z}`;
+    document.getElementById('current').textContent = `現在地の座標
+    x:${currentPos.x}, y:${currentPos.y}, z:${currentPos.z}`;
 
     const distinationPos = document.getElementById('Bamiyan').object3D.position;;
     document.getElementById('distination').textContent = `目的地: ${distinationPos.x}, ${distinationPos.y}, ${distinationPos.z}`;
