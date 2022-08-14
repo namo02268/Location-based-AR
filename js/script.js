db3e199fd02eb5b0ae375c3b06d23ba860e65554
namo <namo02268@gmail.com>
2022-08-15 03:26:49 +0900

    [Add] querySelector

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@ window.addEventListener('load', () => {
   // 1000msごとに実行
   setInterval(() => {
     // カメラ
-    const camera = document.getElementById('camera');
+    const camera = document.querySelector('.camera');
     const cPos = camera.getAttribute('position');
     const cLatLon = camera.components['gps-camera'].currentCoords;
 
-    document.getElementById('cPos').textContent = `x:${cPos.x.toFixed(1)}m, y:${cPos.y.toFixed(1)}m, z:${cPos.z.toFixed(1)}m`;
-    document.getElementById('cLatLon').textContent = `lat:${cLatLon.latitude}, lon:${cLatLon.longitude}`;
+    document.querySelector('.cPos').textContent = `x:${cPos.x.toFixed(1)}m, y:${cPos.y.toFixed(1)}m, z:${cPos.z.toFixed(1)}m`;
+    document.querySelector('.cLatLon').textContent = `lat:${cLatLon.latitude}, lon:${cLatLon.longitude}`;
 
     console.log(camera);
 
     // 目的地
-    const distination = document.getElementById('distination');
+    const distination = document.querySelector('.distination');
     const dPos = distination.getAttribute('position');
     const dLatLon = distination.components['gps-entity-place'].attrValue;
 
-    document.getElementById('dPos').textContent = `x:${dPos.x.toFixed(1)}m, y:${dPos.y.toFixed(1)}m, z:${dPos.z.toFixed(1)}m`;
-    document.getElementById('dLatLon').textContent = `lat:${dLatLon.latitude}, lon:${dLatLon.longitude}`;
+    document.querySelector('.dPos').textContent = `x:${dPos.x.toFixed(1)}m, y:${dPos.y.toFixed(1)}m, z:${dPos.z.toFixed(1)}m`;
+    document.querySelector('.dLatLon').textContent = `lat:${dLatLon.latitude}, lon:${dLatLon.longitude}`;
 
     // 距離
     distance = cPos.distanceTo(dPos);
-    document.getElementById('distance').textContent = `${distance.toFixed(1)} meters`;
+    document.querySelector('.distance').textContent = `${distance.toFixed(1)} meters`;
   }, 1000);
 });
